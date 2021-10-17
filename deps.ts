b2f4413b595074937f012062a744a945c638ff33
Benjamin Fischer <c4spar@gmx.de>
2021-10-17 17:04:56 +0200

    chore(upgrade): deno/std v0.111.0

--- deps.ts
+++ deps.ts
@@ -1,18 +1,18 @@
 export {
   parse as parseYaml,
   stringify as stringifyYaml,
-} from "https://deno.land/std@0.110.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.111.0/encoding/yaml.ts";
 export {
   basename,
   dirname,
   join,
-} from "https://deno.land/std@0.110.0/path/mod.ts";
+} from "https://deno.land/std@0.111.0/path/mod.ts";
 export {
   blue,
   bold,
   dim,
   green,
-} from "https://deno.land/std@0.110.0/fmt/colors.ts";
+} from "https://deno.land/std@0.111.0/fmt/colors.ts";
 export type {
   OpenAPIV3,
   OpenAPIV3_1,
